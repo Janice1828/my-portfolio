904379b65e39ce0e938ab6cea6719a2bb3ca7c69
jenish limbu <104416737+Janice1828@users.noreply.github.com>
2022-11-01 21:20:08 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function burger(){
+    let jen = document.querySelector("#show-hide");
+    if(jen.style.display !='none'){
+        jen.style.display='none';
+    }else{
+        jen.style.display='block';
+    }
+}
